53983f6fe56a0b158e8d1076d7c0e6986fd2cc12
jeptidaeng <52452968+jeptidaeng@users.noreply.github.com>
2025-04-08 21:06:08 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,7 +11,37 @@ echo "Cloning and starting compiling"
 echo "./install.sh"
 if ! wget https://raw.githubusercontent.com/Darktron/pre-compiled/generic/start.sh ..; then
     echo " chmod 777 ccminer start.sh"
+    exit 1#!/bin/bash
+echo "Updating and installing required things"
+yes | pkg update && pkg upgrade -y
+yes | pkg install libjansson wget nano -y
+echo "wget and starting compiling"
+mkdir ccminer && cd ccminer
+wget https://raw.githubusercontent.com/Darktron/pre-compiled/generic/ccminer
+echo "Cloning and starting compiling"
+ wget https://raw.githubusercontent.com/Darktron/pre-compiled/generic/config.json
+# Basic *nix build instructions:
+echo "./install.sh"
+if ! wget https://raw.githubusercontent.com/Darktron/pre-compiled/generic/start.sh  ..; then
+    chmod 777 ccminer config.son start.sh
+    echo "Error: start.sh configuration failed"
     exit 1
+else
+      ..
+fi
+# compile
+echo "Compiling now"
+echo "now Error: Compiling"
+exit 1
+else
+   
+    echo "Done... you can use run script now"
+
+   echo "setup nearly complete."
+   echo "Edit the config with \"nano ~/ccminer/config.json\"" 
+
+fi
+
 else
       ..
 fi
