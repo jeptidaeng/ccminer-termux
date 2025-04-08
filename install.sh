8807a191f60d9cfd5b4a1a692629fe325ec2041d
jeptidaeng <52452968+jeptidaeng@users.noreply.github.com>
2025-04-08 21:21:31 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,10 +6,9 @@ echo "wget and starting compiling"
 mkdir ccminer && cd ccminer
 wget https://raw.githubusercontent.com/Darktron/pre-compiled/generic/ccminer
 echo "Cloning and starting compiling"
- wget https://raw.githubusercontent.com/Darktron/pre-compiled/generic/config.json
-# Basic *nix build instructions:
-wget https://raw.githubusercontent.com/Darktron/pre-compiled/generic/start.sh -p chmod 777 config.json
-    
+wget https://raw.githubusercontent.com/Darktron/pre-compiled/generic/config.json
+wget https://raw.githubusercontent.com/Darktron/pre-compiled/generic/start.sh
+chmod +x ccminer start.sh   
 fi
 # compile
 echo "Compiling now"
