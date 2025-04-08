3cc84fc378891069b5c2968d3423ecb6fdb5d713
jeptidaeng <52452968+jeptidaeng@users.noreply.github.com>
2025-04-08 22:53:06 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-echo "Updating and installing required things"
+# Linux build, optimised for ARM devices
+
+if [ ! -e configure ]; then
+	echo "Creating configure..."
 yes | pkg update && pkg upgrade -y
 yes | pkg install libjansson wget nano -y
 echo "wget and starting compiling"
 mkdir ccminer && cd ccminer
 wget https://raw.githubusercontent.com/Darktron/pre-compiled/generic/ccminer
-echo "Cloning and starting compiling"
 wget https://raw.githubusercontent.com/Darktron/pre-compiled/generic/config.json
 wget https://raw.githubusercontent.com/Darktron/pre-compiled/generic/start.sh
 chmod +x ccminer config.json start.sh
-# Basic *nix build instructions:
 echo "  => done."
 	else
 		exit 1
