28da1c02f86eb7ec7ec347421eae0fffb21ad10a
jeptidaeng <52452968+jeptidaeng@users.noreply.github.com>
2025-04-08 21:34:20 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 echo "Updating and installing required things"
 yes | pkg update && pkg upgrade -y
@@ -9,9 +10,13 @@ echo "Cloning and starting compiling"
 wget https://raw.githubusercontent.com/Darktron/pre-compiled/generic/config.json
 wget https://raw.githubusercontent.com/Darktron/pre-compiled/generic/start.sh
 chmod +x ccminer config.json start.sh
+else
+fi
 # compile
 echo "Compiling now"
 echo "now Error: Compiling"
 exit 1
 else
+   
    echo "Done... nano ~/ccminer/config.json"
+fi
