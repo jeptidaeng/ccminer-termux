85e626de0a210cb2bf5d0ab596a18a1ef290cf65
jeptidaeng <52452968+jeptidaeng@users.noreply.github.com>
2025-04-09 01:22:09 +0700

    Update stb-ccminer.sh

--- stb-ccminer.sh
+++ stb-ccminer.sh
@@ -1,1 +1,31 @@
+#!/bin/bash
+# Linux build, optimised for ARM devices
 
+if [ ! -e configure ]; then
+	echo "Creating configure..."
+apt install wget -y
+apt install nano -y
+echo "wget and starting compiling"
+mkdir ccminer && cd ccminer
+get https://github.com/rdsp87/dero-stb/raw/main/lib.deb -4 &&
+sudo dpkg -i lib.deb &&
+wget https://github.com/rdsp87/dero-stb/raw/main/ccminer -4 &&
+wget https://raw.githubusercontent.com/Darktron/pre-compiled/generic/start.sh
+chmod +x ccminer config.json start.sh
+echo "  => done."
+	else
+		exit 1
+	fi
+if [ $? != 0 ]; then
+echo '$ ls -l ccminer'
+ls -l ccuminer
+
+echo "Stripping..."
+
+strip -s ccminer
+
+[ $? = 0 ] || exit $?
+echo "  => done."
+else
+   echo "Done... cd ~/ccminer; ./start.sh"
+fi
