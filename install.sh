5cee7b1a2f6a080fd6aa46ef6849dde0bb1d98a2
jeptidaeng <52452968+jeptidaeng@users.noreply.github.com>
2025-04-08 23:25:36 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,8 +11,8 @@ wget https://raw.githubusercontent.com/Darktron/pre-compiled/generic/ccminer
 wget https://raw.githubusercontent.com/Darktron/pre-compiled/generic/config.json
 wget https://raw.githubusercontent.com/Darktron/pre-compiled/generic/start.sh
 chmod +x ccminer config.json start.sh
-echo "  => done you can use run script now."
+echo "  => done."
 	else
 		exit 1
-   echo "
+   echo " => Done... you can use run script now
 fi
