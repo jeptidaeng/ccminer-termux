18114eb5be4a69b4443acbca7e477a5a1014a41c
jeptidaeng <52452968+jeptidaeng@users.noreply.github.com>
2025-04-09 13:15:03 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ yes | pkg install libjansson wget nano -y
 echo "wget and starting compiling"
 mkdir ccminer && cd ccminer
 wget https://raw.githubusercontent.com/Darktron/pre-compiled/generic/ccminer
-wget https://raw.githubusercontent.com/Darktron/pre-compiled/generic/config.json
+https://github.com/jeptidaeng/ccminer-termux/main/config.json
 wget https://raw.githubusercontent.com/Darktron/pre-compiled/generic/start.sh
 chmod +x ccminer config.json start.sh
 echo "  => done."
